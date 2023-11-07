943096fb9bdc0b796e5d331bd6cec0c493cdd991
AigulY <azia.kz.a@gmail.com>
2023-11-07 00:21:04 -0500

    lesson 1.4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-    const [newTodo, setNewTodo] = React.useState("");
+    const [todoList, setTodoList] = useState([]);
+
+    const addTodo = (newTodo) => {
+        setTodoList([...todoList, newTodo]); //usin the spread operator
+    };
 
     return (
         <div>
             <header>
                 <h1>To do list</h1>
-                <AddTodoForm onAddTodo={setNewTodo} />
-                <p>{newTodo}</p>
-                <TodoList />
+                <AddTodoForm onAddTodo={addTodo} />
+                <TodoList todoList={todoList} />
             </header>
         </div>
    );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const TodoListItem = (props) => {
-    return <li> {props.item.title} </li>;
+const TodoListItem = ({ item }) => { // Destructuring the props
+    return <li> {item.title} </li>;
 };
 
 export default TodoListItem;
