72d22cc84f92a6d2be2f98a0c166a0e91436eb9b
AigulY <azia.kz.a@gmail.com>
2024-02-13 01:08:36 -0500

    added Updated functionality, fixed error, modified header, added Media Queries, customized styles of Navigation menu

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import TodoContainer from './components/TodoContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Notification from './components/AlertMessage';
 import TodoUpdateForm from './components/TodoUpdateForm';
+import Navbar from './components/Navbar';
 import './App.css';
-
 
 function App() {
     const [notification, setNotification] = useState({ message: '', type: '' });
 
     const showErrorNotification = (message) => {
-        setNotification({ message, type: 'error' })
+        setNotification({ message, type: 'error' });
     }
 
     const showSuccessNotification = (message) => {
-        setNotification({ message, type: 'success' })
+        setNotification({ message, type: 'success' });
     }
 
     return (
         <BrowserRouter>
           <Header />
-          {notification.message && <Notification message={notification.message} type={notification.type} />}
-          <Routes>
-            <Route path="/" element={
-              <TodoContainer 
-                showErrorNotification={showErrorNotification} 
-                showSuccessNotification={showSuccessNotification} 
-              />
-            } />
-            <Route path="/new" element={
+            <Navbar />
+                {notification.message && <Notification message={notification.message} type={notification.type} />}
+            <Routes>
+                <Route path="/" element={
+                <TodoContainer 
+                    showErrorNotification={showErrorNotification} 
+                    showSuccessNotification={showSuccessNotification} 
+                />
+                } />
+                <Route path="/new" element={
                     <header>
                         <h1>New Todo List</h1>
                     </header>
                 } />
-            <Route path="/update/:id" element={<TodoUpdateForm />} />
+                <Route path="/update/:id" element={<TodoUpdateForm/>} />
             </Routes>
         </BrowserRouter>
-      );
-    }
+    );
+}
 export default App;
